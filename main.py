8963af89fdebebba8d816cc49814cc52293ccc32
gofaquan <2660796265@qq.com>
2022-05-25 17:30:29 +0800

    :tada: run whole the project & v1.0 done

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from spider import weather_spider
-from spider.range import rangeTime
-import save_data as db
+from db.save_data import insert
 
 if __name__ == "__main__":
-    ret = rangeTime(2021, 3, 2021, 5)
-
-    for a, b in ret:
-        print(a)
-        print(b)
-    # print(weather_condition_dict)
-    # print('-------------------------------------')
-    # print(month_dict)
-    # print('-------------------------------------')
-
-    # # print(a.month_dict)
-    # conn, cu = db.connectDB()
-    # db.insertData(cu, weather_condition_dict,month_dict)
-    #
-    # db.commit(conn)
+    insert(2022, 1, 2022, 4)
