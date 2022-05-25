4c27c10e05587655bdd9828feb3cee355c9b6928
gofaquan <2660796265@qq.com>
2022-05-25 16:55:26 +0800

    :zap: add func to get diy data

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from spider import weather_spider
+from spider.range import rangeTime
 import save_data as db
 
 if __name__ == "__main__":
-    a = weather_spider.WeatherSpider()
-    weather_condition_dict, month_dict = a.getData()
+    ret = rangeTime(2021, 3, 2021, 5)
 
-    # print(a.month_dict)
-    conn, cu = db.connectDB()
-    db.insertData(cu, a)
+    for a, b in ret:
+        print(a)
+        print(b)
+    # print(weather_condition_dict)
+    # print('-------------------------------------')
+    # print(month_dict)
+    # print('-------------------------------------')
 
-    db.commit(conn)
+    # # print(a.month_dict)
+    # conn, cu = db.connectDB()
+    # db.insertData(cu, weather_condition_dict,month_dict)
+    #
+    # db.commit(conn)
---
+++ ye.py
@@ -0,0 +1,6 @@
+kv = {'key1':'value1','key2':'value2'}
+
+print(",".join(["%s" for _ in kv.keys()]))
+
+for i in range(3):
+    print(i)
