e303a1012fa077ac90185f03d86840af996e5e4e
gofaquan <2660796265@qq.com>
2022-05-28 13:25:49 +0800

    :sparkles: add new func to gen day data html

--- db/read_data.py
+++ db/read_data.py
@@ -2,10 +2,12 @@ import pymysql
 
 
 # 读取月度数据
-def read_month_data(cu):
-    date_sql = 'select 日期 from 每月数据;'
-    a_t_sql = 'select 平均高温 from 每月数据;'
-    a_b_sql = 'select 平均低温 from 每月数据;'
+def read_month_data_2021(cu):
+    date_sql = "select 日期 from 每月数据 where 日期 like '2021%';"
+    a_t_sql = "select 平均高温 from 每月数据 where 日期 like '2021%';"
+    t_sql = "select 极端高温 from 每月数据 where 日期 like '2021%' ;"
+    a_b_sql = "select 平均低温 from 每月数据 where 日期 like '2021%' ;"
+    b_sql = "select 极端低温 from 每月数据 where 日期 like '2021%' ;"
 
     cu.execute(date_sql)
     x_result = cu.fetchall()
@@ -16,4 +18,17 @@ def read_month_data(cu):
     cu.execute(a_b_sql)
     a_b = list(cu.fetchall())
 
-    return x_result, a_t, a_b
+    cu.execute(t_sql)
+    t = list(cu.fetchall())
+
+    cu.execute(b_sql)
+    b = list(cu.fetchall())
+
+    return x_result, a_t, a_b, t, b
+
+
+def read_day_data_2021(cu):
+    sql = "select 最高温 from 每日数据 where 日期 like '2021%' ;"
+    cu.execute(sql)
+    result = cu.fetchall()
+    return result
--- echarts/gen_chart.py
+++ echarts/gen_chart.py
@@ -2,14 +2,53 @@ from pyecharts.charts import Bar
 from pyecharts import options as opts
 # 内置主题类型可查看 pyecharts.globals.ThemeType
 from pyecharts.globals import ThemeType
+import random
+import datetime
+from pyecharts.charts import Calendar
 
 
-def create_chart(x, y1, y2):
+def create_chart(x, y1, y2, y3, y4):
     bar = (
         Bar(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
             .add_xaxis(x)
             .add_yaxis("平均高温", y1)
             .add_yaxis("平均低温", y2)
-            .set_global_opts(title_opts=opts.TitleOpts(title="历年天气", subtitle="气温"))
+            .add_yaxis("最高温", y3)
+            .add_yaxis("最低温", y4)
+            .set_global_opts(title_opts=opts.TitleOpts(title="历年天气", subtitle="气温"),
+                             xaxis_opts=opts.AxisOpts(
+                                 is_show=False,  # 隐藏X轴刻度
+                             ), )
     )
     bar.render()
+
+
+def calendar_day(data):
+    begin = datetime.date(2021, 1, 1)
+    end = datetime.date(2021, 12, 31)
+    data = [
+        [str(begin + datetime.timedelta(days=i)), data[i]]
+        for i in range((end - begin).days + 1)
+    ]
+
+    (
+        Calendar(init_opts=opts.InitOpts(width="1600px", height="1000px"))
+            .add(
+            series_name="",
+            yaxis_data=data,
+            calendar_opts=opts.CalendarOpts(
+                pos_top="120",
+                pos_left="30",
+                pos_right="30",
+                range_="2021",
+                yearlabel_opts=opts.CalendarYearLabelOpts(is_show=False),
+            ),
+        )
+            .set_global_opts(
+            title_opts=opts.TitleOpts(pos_top="30", pos_left="center", title="2021年最高温情况"),
+            visualmap_opts=opts.VisualMapOpts(
+                max_=45, min_=5, orient="horizontal", is_piecewise=False
+            ),
+        )
+            .render("每日数据最高温.html")
+    )
