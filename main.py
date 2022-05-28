13f7a5fd265839f580446b002133b3a56d2d385f
gofaquan <2660796265@qq.com>
2022-05-28 13:26:36 +0800

    :tada: successfully gen 2 html file to show the charts

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from db.save_data import insert, read_month
-from echarts.gen_chart import create_chart
+from db.save_data import insert, read_month, read_day
+from echarts.gen_chart import create_chart,calendar_day
 from echarts.get_temperature import spilt_flag
 
 if __name__ == "__main__":
     # insert(2021, 1, 2022, 4)
-    x, y1, y2 = read_month()
+    x, y1, y2, y3, y4 = read_month()
     print(y1)
-    y1 = spilt_flag(y1)
-    print(y1)
-    # create_chart(x, y1, y2)
 
+    create_chart(x, spilt_flag(y1), spilt_flag(y2), spilt_flag(y3), spilt_flag(y4))
+
+    day_t = read_day()
+    print(spilt_flag(day_t))
+    calendar_day(spilt_flag(day_t))
